26bab8ee747a3a298eafa165702b07c8df59a64e
Parth Mall <parthmall@iitkgp.ac.in>
2021-03-22 00:52:54 +0530

    Add precise intructions to README, add mandatory keyword in regitration form

--- airlinemgmt/users/forms.py
+++ airlinemgmt/users/forms.py
@@ -6,15 +6,15 @@ from flask_login import current_user
 from airlinemgmt.models import User
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    address = TextAreaField('Address', validators=[DataRequired()])
-    pincode = StringField('Pincode', validators=[DataRequired(), Length(min=6, max=6)])
-    dob = DateField('Date of Birth', validators=[DataRequired()])
+    name = StringField('Name (*)', validators=[DataRequired()])
+    email = StringField('Email (*)', validators=[DataRequired(), Email()])
+    address = TextAreaField('Address (*)', validators=[DataRequired()])
+    pincode = StringField('Pincode (*)', validators=[DataRequired(), Length(min=6, max=6)])
+    dob = DateField('Date of Birth (yyyy-mm-dd) (*)', validators=[DataRequired()])
     phnum = StringField('Phone number', validators=[Length(min=10, max=10)])
     gender = SelectField('Gender', choices=[('M', 'Male'), ('F', 'Female'), ('x', 'Rather not say')])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Password (*)', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password (*)', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, email):

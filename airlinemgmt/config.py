d2c38684d32a5826ca96c3475a3b0ef0fcf2611c
Parth Mall <parthmall@iitkgp.ac.in>
2021-03-30 09:38:08 +0530

    config update

--- airlinemgmt/config.py
+++ airlinemgmt/config.py
@@ -6,5 +6,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'dparthp@gmail.com'
-    MAIL_PASSWORD = 'judge=madarchod'
+    MAIL_USERNAME = 'dbmsairlines@gmail.com'
+    MAIL_PASSWORD = 'MNyxaBAVtyXjTg6'
